8e7ffb4b8f099b7da3ef099324b0e637fd6942c4
Jonas Roer <jonroe01@easv365.dk>
2023-12-19 23:23:43 +0100

    fix time format

--- src/sample/BE/Playlist.java
+++ src/sample/BE/Playlist.java
@@ -91,7 +91,7 @@ public class Playlist {
             int hours = totalSecs / 3600;
             int minutes = (totalSecs % 3600) / 60;
             int seconds = totalSecs % 60;
-            return String.format("%d:%d:%d", hours, minutes, seconds);
+            return String.format("%02d:%02d:%02d", hours, minutes, seconds);
     }
 
     @Override
